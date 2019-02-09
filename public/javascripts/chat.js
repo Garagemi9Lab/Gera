d9cf449adfb7337b7b7e58ebc016b6615230cae0
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2019-02-09 16:34:10 -0200

    remover botoes de venda condicioanda

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -415,26 +415,16 @@ function disableCollapsibleItems() {
         let collapsible_body = collapsible_item.childNodes[1]
         if (collapsible_body)
             collapsible_body.remove()
-
-        // let collapsible_subitems = collapsible_body.childNodes
-        // for (var j = 0; j < collapsible_subitems.length; j++) {
-        // let collapsible_subitem = collapsible_subitems[j]
-        // collapsible_subitem.removeAttribute('id')
-        // let collapsible_input_holder = collapsible_subitem.childNodes[2]
-        // let itemsToDisable = collapsible_input_holder.childNodes
-        // for (var k = 1; k < itemsToDisable.length; k++) {
-        // if (!itemsToDisable[k].disabled) {
-        // itemsToDisable[k].disabled = true
-        // itemsToDisable[k].removeAttribute('id')
-        // }
-        // }
-
-
-
         let collapsible_header = collapsible_item.childNodes[0]
         let arrow_div = collapsible_header.childNodes[1]
         if (arrow_div)
             arrow_div.remove()
+    }
+
+    // remove btns
+    let collapsible_btns = document.getElementsByClassName('collapsible-btns-holder')
+    for (var i = 0; i < collapsible_btns.length; i++) {
+        if (collapsible_btns[i]) collapsible_btns[i].remove()
     }
 }
 
