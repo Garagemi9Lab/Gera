253fd30751fe18febe60cab7202def9f1d888f2e
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2019-05-09 14:42:26 -0300

    fix for both env

---
+++ environments/index.js
@@ -0,0 +1,20 @@
+var chalk = require('chalk')
+var fs = require('fs')
+var path = require('path')
+
+
+
+console.log(chalk.cyan('Copying local environments'))
+
+var env_app = process.env.APP_ENV
+
+if (env_app) {
+    fs.readFile(path.join(__dirname, `.${env_app}.env`), (err, data) => {
+        if (err) throw err
+
+        fs.writeFile('.env', data, (err) => {
+            if (err) throw err
+            console.log(chalk.green('Environments file copied successfully'))
+        })
+    })
+}
